d8ffd0282a32e08a1d4896e9d8e69a2caa63f204
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-05-12 15:56:30 -0300

    Ajust import and call to method routers which returns a http instance

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"github.com/dimfeld/httptreemux"
 	"github.com/kauehmoreno/microservico-golang/router"
-	"gopkg.in/mgo.v2"
 	"log"
 	"net/http"
 )
 
 func main() {
-	router = router.routers()
-	address := "127.0.0.0:8081"
+	rt := router.Routers()
+	address := "127.0.0.1:8081"
 	log.Printf("Running api restfull on : http://%s\n", address)
-	log.Fatal(http.ListenAndServe(address, router))
+	log.Fatal(http.ListenAndServe(address, rt))
 
 }
