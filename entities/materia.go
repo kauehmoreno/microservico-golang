e105ddbe0a01e487287e3f2b66b54c51f2fd6e28
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-05-15 10:38:35 -0300

    Alter json/bson contract

--- entities/materia.go
+++ entities/materia.go
@@ -4,9 +4,9 @@ type Materia struct {
 	Titulo          string `json bson:"titulo"`
 	Subtitulo       string `json bson: "subtitulo"`
 	Cover           string `json bson:"cover"`
-	DataCriacao     string `json bson:"data_criacao"`
-	DataModificacao string `json bson:"data_modificacao"`
-	DataPublicacao  string `json bson:"data_publicacao"`
+	DataCriacao     string `json bson:"datacriacao"`
+	DataModificacao string `json bson:"datamodificacao"`
+	DataPublicacao  string `json bson:"datapublicacao"`
 	Slug            string `json bson:"slug"`
 	Id              string `json bson:"id"`
 	Corpo           string `json bson:"corpo"`
